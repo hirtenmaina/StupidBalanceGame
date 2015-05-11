7c81ef09094b0007befb8e94228c8f068bd18c56
Tatiana <sttatrub@stud.h-da.de>
2015-05-11 13:48:11 +0200

    Code Fixed

--- Assets/Scripts/CollisionTest.cs
+++ Assets/Scripts/CollisionTest.cs
@@ -7,15 +7,16 @@ public class CollisionTest : MonoBehaviour {
 	private Color cubeColor;
 
 	
-	void Update (){	
-		//cubeColor = gameObject.GetComponent<Renderer> ().material.color;
+	void Update(){	
+
 		cubeColor = Color.cyan;
 		if (interval > 0 && this.gameObject.GetComponent<Renderer>().material.color == cubeColor) {
 			interval -= Time.deltaTime;
-		} else {
-		
+			Debug.Log("Timer set");
+		} else if (interval <= 0 && this.gameObject.GetComponent<Renderer>().material.color == cubeColor) {
+	
 			gameObject.GetComponent<Renderer>().material.color = Color.red;
-			Debug.Log ("colour set to RED");
+			Debug.Log ("RED");
 		}
 	}
 
@@ -23,9 +24,11 @@ public class CollisionTest : MonoBehaviour {
 	void OnCollisionEnter() {
 		
 		gameObject.GetComponent<Renderer>().material.color = Color.cyan;
-		Debug.Log ("Collided Cube");	
-		Debug.Log(interval);
+		Debug.Log ("Collided Cube");
+		interval = 5f;
+
 	}
 	
 
 }
+
